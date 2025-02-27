2e800ea7f53260ae726a4445abe6ae186fa986c4
tanulo <tanulo@barossgabor.local>
2025-02-27 09:10:21 +0100

    script.js valtozas keyup event

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
     textarea.name='area'
     textarea.id='area'
     textarea.placeholder='Írd be a jelszavad:'
-    textarea.addEventListener('keypress', ()=> jelszo(textarea, demoSpan))
+    textarea.addEventListener('keyup', ()=> jelszo(textarea, demoSpan))
 
     containerDiv.append(demoSpan, textarea)
 
@@ -17,12 +17,13 @@ window.addEventListener('DOMContentLoaded', ()=>{
     document.body.append(containerDiv)
 })
 
-function jelszo(textare, demoSpan) {
+function jelszo(textarea, demoSpan) {
     let csillag='';
 
     for (let i = 0; i < textarea.value.length; i++) {
+        csillag+='*'
         
-
-
     }
+
+    demoSpan.textContent=csillag
 }
